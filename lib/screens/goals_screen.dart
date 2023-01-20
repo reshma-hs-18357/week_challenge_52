a7695cf6761e5c60afff85c8f0f8c17348defb15
reshma-14 <reshma.hs@zohocorp.com>
2023-01-20 11:48:36 +0530

    Week 52

--- lib/screens/goals_screen.dart
+++ lib/screens/goals_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:week_challenge_52/components/goal_card.dart';
-import 'package:week_challenge_52/components/text_card.dart';
 import 'package:week_challenge_52/components/title_card.dart';
 import 'package:week_challenge_52/models/goal.dart';
 import 'package:week_challenge_52/screens/home_screen.dart';
 
-class GoalsScreen extends StatefulWidget {
+class GoalsScreen extends StatelessWidget {
   final List<Goal> goalList;
-  final void Function(Goal? goal) onCardTapped;
+  final void Function(Goal goal) onCardTapped;
   final Filter filter;
   const GoalsScreen({
     super.key,
@@ -15,26 +14,13 @@ class GoalsScreen extends StatefulWidget {
     required this.onCardTapped,
     required this.filter,
   });
-
-  @override
-  State<GoalsScreen> createState() => _GoalsScreenState();
-}
-
-class _GoalsScreenState extends State<GoalsScreen> {
-  List<dynamic> listItems = [];
-  @override
-  void initState() {
-    listItems = _prepareListItems();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return _goalScreenBody();
   }
 
   Widget _goalScreenBody() {
-    if (widget.goalList.isEmpty) {
+    if (goalList.isEmpty) {
       return Column(
         children: [
           const SizedBox(height: 60),
@@ -62,7 +48,8 @@ class _GoalsScreenState extends State<GoalsScreen> {
         ],
       );
     } else {
-      if (widget.filter == Filter.all) {
+      if (filter == Filter.all) {
+        List<dynamic> listItems = _prepareListItems();
         return ListView.builder(
           itemCount: listItems.length,
           itemBuilder: ((context, index) {
@@ -73,19 +60,19 @@ class _GoalsScreenState extends State<GoalsScreen> {
               Goal goal = listItems[index];
               return GoalCard(
                 goal: goal,
-                onCardTapped: widget.onCardTapped,
+                onCardTapped: onCardTapped,
               );
             }
           }),
         );
       } else {
         return ListView.builder(
-          itemCount: widget.goalList.length,
+          itemCount: goalList.length,
           itemBuilder: ((context, index) {
-            Goal goal = widget.goalList[index];
+            Goal goal = goalList[index];
             return GoalCard(
               goal: goal,
-              onCardTapped: widget.onCardTapped,
+              onCardTapped: onCardTapped,
             );
           }),
         );
@@ -94,13 +81,14 @@ class _GoalsScreenState extends State<GoalsScreen> {
   }
 
   List<dynamic> _prepareListItems() {
+    List<dynamic> listItems = [];
     List<Goal> weeklyGoals = [];
     List<Goal> monthlyGoals = [];
-    for (int i = 0; i < widget.goalList.length; i++) {
-      if (widget.goalList[i].savingsChoice == SavingsChoice.weekly) {
-        weeklyGoals.add(widget.goalList[i]);
+    for (int i = 0; i < goalList.length; i++) {
+      if (goalList[i].savingsChoice == SavingsChoice.weekly) {
+        weeklyGoals.add(goalList[i]);
       } else {
-        monthlyGoals.add(widget.goalList[i]);
+        monthlyGoals.add(goalList[i]);
       }
     }
     listItems.add("Weekly Goals");
